3c1f6427f63428f48b9f70efb2aa3a71e0871bef
Carlos Teofilo <carlosneto2810@hotmail.com>
2026-02-18 17:21:01 -0400

    Add new fields to User model

--- src/Models/User.cs
+++ src/Models/User.cs
@@ -4,4 +4,6 @@ public class User
 {
     public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
+    public string Email { get; set; }
+    public string Password { get; set; }
 }
