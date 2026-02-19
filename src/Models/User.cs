d166f430212d4c3a0b3aa8d8201a09f67f8a8f11
Carlos Teofilo <carlosneto2810@hotmail.com>
2026-02-18 21:40:12 -0400

    * Add new fields to User: - Slug - IsActive - LastLoginAt - CreatedAt

--- src/Models/User.cs
+++ src/Models/User.cs
@@ -3,9 +3,13 @@ namespace Api.Models;
 public class User
 {
     public int Id { get; set; }
-    public string Name { get; set; } = string.Empty;
-    public string Email { get; set; }
-    public string PasswordHash { get; set; }
+    public required string Name { get; set; }
+    public required string Email { get; set; }
+    public required string PasswordHash { get; set; }
+    public required string Slug { get; set; }
+    public bool IsActive { get; set; } = false;
+    public DateTime? LastLoginAt { get; set; }
+    public DateTime CreatedAt { get; private set; } = DateTime.UtcNow;
 
     public IList<UserMembership> Memberships { get; set; } = new List<UserMembership>();
 }
