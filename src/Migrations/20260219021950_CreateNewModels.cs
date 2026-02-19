ab19e7d24f8041d905fee769d51532bf2ff91b58
Carlos Teofilo <carlosneto2810@hotmail.com>
2026-02-18 22:21:35 -0400

    * Modified models 	- Membership 	- User * Rename Model 	- UserMembership to Subscription

---
+++ src/Migrations/20260219021950_CreateNewModels.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace app.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateNewModels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserMembership");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Membership",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Membership",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "DurationInMonths",
+                table: "Membership",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Membership",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Membership",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Subscription",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UserId = table.Column<int>(type: "integer", nullable: false),
+                    MembershipId = table.Column<int>(type: "integer", nullable: false),
+                    DueDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    ExpiresAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Status = table.Column<int>(type: "integer", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Subscription", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Subscription_Membership_MembershipId",
+                        column: x => x.MembershipId,
+                        principalTable: "Membership",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Subscription_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Subscription_MembershipId",
+                table: "Subscription",
+                column: "MembershipId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Subscription_UserId",
+                table: "Subscription",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Subscription");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Membership");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Membership");
+
+            migrationBuilder.DropColumn(
+                name: "DurationInMonths",
+                table: "Membership");
+
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Membership");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "Membership");
+
+            migrationBuilder.CreateTable(
+                name: "UserMembership",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    MembershipId = table.Column<int>(type: "integer", nullable: false),
+                    UserId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserMembership", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserMembership_Membership_MembershipId",
+                        column: x => x.MembershipId,
+                        principalTable: "Membership",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserMembership_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserMembership_MembershipId",
+                table: "UserMembership",
+                column: "MembershipId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserMembership_UserId",
+                table: "UserMembership",
+                column: "UserId");
+        }
+    }
+}
