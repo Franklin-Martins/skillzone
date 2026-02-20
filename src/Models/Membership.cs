aed7c933351a049629728729e433d8a34b7a54da
Carlos Teofilo <carlosneto2810@hotmail.com>
2026-02-20 19:37:06 -0400

    Update field  to

--- src/Models/Membership.cs
+++ src/Models/Membership.cs
@@ -7,7 +7,7 @@ public class Membership
     public required int Price { get; set; }
     public string? Description { get; set; }
     public required int DurationInMonths { get; set; }
-    public bool IsActive { get; set; } = false;
+    public bool IsActive { get; private set; } = false;
     public DateTime? UpdatedAt { get; private set; }
     public DateTime CreatedAt { get; private set; } = DateTime.UtcNow;
 
