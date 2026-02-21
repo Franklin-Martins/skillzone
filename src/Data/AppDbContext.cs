626bef3bfce0b431b10a47d662a03214559c20e6
Carlos Teofilo <carlosneto2810@hotmail.com>
2026-02-20 20:43:44 -0400

    Update

--- src/Data/AppDbContext.cs
+++ src/Data/AppDbContext.cs
@@ -8,5 +8,10 @@ public class AppDbContext : DbContext
     public AppDbContext(DbContextOptions<AppDbContext> options)
         : base(options) { }
 
-    public DbSet<User> Users => Set<User>();
+    public DbSet<User> Users { get; set; }
+    public DbSet<UserContentProgress> UserContentProgresses { get; set; }
+    public DbSet<Content> Contents { get; set; }
+    public DbSet<Membership> Memberships { get; set; }
+    public DbSet<MembershipContent> MembershipContents { get; set; }
+    public DbSet<Subscription> Subscriptions { get; set; }
 }
