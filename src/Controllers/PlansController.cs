50acd428c468ae1d9a7e615e03841369c073a68b
Carlos Teofilo <carlosneto2810@hotmail.com>
2026-03-08 09:30:02 -0400

    Building Plan

---
+++ src/Controllers/PlansController.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+[ApiController]
+[Route("api/[controller]")]
+public class PlansController : ControllerBase
+{
+
+}
--- src/Models/Plan.cs
+++ src/Models/Plan.cs
@@ -5,16 +5,16 @@ public class Plan
     public int Id { get; set; }
 
     public User User { get; set; } = null!;
-    public Guid UserId { get; set; }
+    public Guid? UserId { get; set; }
 
-    public bool IsOwner { get; set; }
-    public required string Name { get; set; }
-    public required int Price { get; set; }
-    public required string Description { get; set; }
-    public required int DurationInMonths { get; set; }
-    public required DateTime DueDate { get; set; }
-    public EPlanStatus Status { get; set; }
-    public DateTime ExpiresAt { get; set; }
+    public bool? IsOwner { get; set; }
+    public string Name { get; set; }
+    public int Price { get; set; }
+    public string Description { get; set; }
+    public int? DurationInMonths { get; set; }
+    public DateTime? DueDate { get; set; }
+    public EPlanStatus? Status { get; set; }
+    public DateTime? ExpiresAt { get; set; }
     public DateTime? UpdatedAt { get; set; }
     public DateTime CreatedAt { get; private set; } = DateTime.UtcNow;
 }
